9832825e0b56980b1a86aad9798a40dd51a1785e
josiahpeters <josiahpeters@gmail.com>
2013-02-25 17:07:02 -0700

    Added Interface for retrieving and caching API data. Started implementing it in a new library CCBoise.Data. Also created the default CCBoise.js file that the APP will use to display the application structure.

---
+++ src/CCBoise.Core/IApiSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CCBoise.Core
+{
+    public interface IApiSource
+    {
+        List<ApiElement> GetElements(string name);
+        List<ApiElement> GetElementDetail(string name, string identifier);
+    }
+
+    public class ApiElement
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string SiteUrl { get; set; }
+
+        private Dictionary<string, object> items;
+
+        public object this[string key]
+        {
+            get
+            {
+                return items[key];
+            }
+        }
+    }
+}
---
+++ src/CCBoise.Data/WebApi.cs
@@ -0,0 +1,50 @@
+﻿using CCBoise.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CCBoise.Data
+{
+    public class WebApi : IApiSource
+    {
+        private Dictionary<string, WebApiEndpoint> endpoints;
+
+        public List<ApiElement> GetElements(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ApiElement> GetElementDetail(string name, string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WebApi()
+        {
+            endpoints = new Dictionary<string, WebApiEndpoint>()
+            {
+                { "video", new WebApiEndpoint { Name = "video", Url = "http://www.ccboise.org/api/messages/video" } },
+                { "audio", new WebApiEndpoint { Name = "audio", Url = "http://www.ccboise.org/api/messages/audio" } },
+                { "devotional", new WebApiEndpoint { Name = "devotional", Url = "http://www.ccboise.org/api/daily/devotionals", ItemUrl = "http://www.ccboise.org/api/daily/devotional/{ItemId}" } },
+                { "prayer", new WebApiEndpoint { Name = "prayer", Url = "http://www.ccboise.org/api/daily/prayer" } },
+                { "events", new WebApiEndpoint { Name = "events", Url = "http://www.ccboise.org/api/connect/events" } },
+                { "calendar", new WebApiEndpoint { Name = "calendar", Url = "http://www.ccboise.org/api/connect/calendar", ItemUrl = "http://www.ccboise.org/api/connect/calendar-event/{ItemId}" } },
+            };
+        }
+
+        private string getUrlContent(string url)
+        {
+            //using(WebClient 
+        }
+    }
+
+    internal class WebApiEndpoint
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string ItemUrl { get; set; }
+        public ApiElement Element { get; set; }
+        public List<ApiElement> ChildElements { get; set; }
+    }
+}
