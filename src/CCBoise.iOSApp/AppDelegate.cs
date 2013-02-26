795b9256c099e214d77263d0225a33e25b7af301
Josiah Peters <josiahpeters@gmail.com>
2013-02-26 09:02:10 -0700

    Cleaning up solution file.

--- src/CCBoise.iOSApp/AppDelegate.cs
+++ src/CCBoise.iOSApp/AppDelegate.cs
@@ -17,7 +17,7 @@ namespace CCBoise.iOSApp
 		UIWindow window;
 		UINavigationController navigationController;
 		UIViewController viewController;
-
+		
 		//
 		// This method is invoked when the application has loaded and is ready to run. In this 
 		// method you should instantiate the window, load the UI into it and then make the window
